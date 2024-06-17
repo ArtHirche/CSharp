5f0f79ba70c1ae30e4d995e5932d46f32c0467a0
ArtHirche <hirche.arthur@gmail.com>
2024-06-17 12:14:35 -0300

    Primeiro contato

---
+++ OlaMundo/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Program
+{
+    public static void Main(string[] argd)
+    {
+
+        Console.WriteLine("Hello, World!"); // output: Hello, World
+
+        var a = 3;
+        Console.WriteLine(a); // output: 3
+        Console.WriteLine(a++); // output: 3
+        Console.WriteLine(a); // output: 4
+
+        
+        int b = 3;
+        Console.WriteLine(b); // output: 3
+        Console.WriteLine(b--); // output: 3
+        Console.WriteLine(b); // output: 2
+
+        
+        int a1 = 1 + 2 + 3;
+        int b1 = 6;
+        Console.WriteLine(a1 == b1); // output: True
+        Console.WriteLine(a1 != b1); // output: False
+
+
+
+        Console.WriteLine(a.GetType()); // output: System.Int32
+
+    
+    }
+
+}
